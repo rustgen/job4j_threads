b925b8ec2398f158badb286c356130037f581226
new_user <nrustgen@gmail.com>
2022-09-21 02:21:45 -0700

    4. Скачивание файла с ограничением. [#144271 #345442]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.net.*;
 
 public class Wget implements Runnable {
-    private static final int SIZE_LIMIT = 1048576;
     private final String url;
     private final String file;
     private final int speed;
@@ -25,19 +24,22 @@ public class Wget implements Runnable {
         }
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
              FileOutputStream fos = new FileOutputStream(file)) {
-            byte[] dataBuffer = new byte[SIZE_LIMIT];
+            byte[] dataBuffer = new byte[1048576];
             int bytesRead;
             long startTime = System.currentTimeMillis();
             int downloadData = 0;
-            while ((bytesRead = in.read(dataBuffer, 0, SIZE_LIMIT)) != -1) {
-                downloadData++;
-                long afterTime = System.currentTimeMillis();
-                long difference = afterTime - startTime;
-                fos.write(dataBuffer, 0, bytesRead);
-                if (difference < 1000) {
-                    Thread.sleep(1000 - difference);
+            while ((bytesRead = in.read(dataBuffer, 0, 1048576)) != -1) {
+                System.out.println(bytesRead);
+                downloadData += bytesRead;
+                if (downloadData >= speed) {
+                    long interval = System.currentTimeMillis() - startTime;
+                    if (interval < 1000) {
+                        Thread.sleep(1000 - interval);
+                    }
                     downloadData = 0;
+                    startTime = System.currentTimeMillis();
                 }
+                fos.write(dataBuffer, 0, bytesRead);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -57,6 +59,9 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
+        if (args.length < 3) {
+            throw new IllegalArgumentException("There are should be 3 parameter for launch application.");
+        }
         String url = args[0];
         String file = args[1];
         int speed = Integer.parseInt(args[2]);
