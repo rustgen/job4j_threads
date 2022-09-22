18cb4c62c60b57f01173b7539b9a46b1cd37aeed
new_user <nrustgen@gmail.com>
2022-09-22 01:54:35 -0700

    3. Денежные переводы AccountStorage [#1104 #345643]

--- src/main/java/ru/job4j/cash/AccountStorage.java
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -11,17 +11,10 @@ public class AccountStorage {
     }
 
     public synchronized boolean update(Account account) {
-        if (getById(account.getId()).isEmpty()) {
-            throw new IllegalArgumentException(String.format(
-                    "There is no such account - \"%s\" with same id among accounts.", account));
-        }
         return accounts.replace(account.getId(), accounts.get(account.getId()), account);
     }
 
     public synchronized boolean delete(int id) {
-        if (getById(id).isEmpty()) {
-            throw new IllegalArgumentException(String.format("There is no such id = %s among accounts.", id));
-        }
         return accounts.remove(id, accounts.get(id));
     }
 
@@ -30,35 +23,12 @@ public class AccountStorage {
     }
 
     public boolean transfer(int fromId, int toId, int amount) {
-        if (getById(fromId).isEmpty()) {
-            throw new IllegalArgumentException(String.format("There is no account with such id = %s number.", fromId));
-        }
-        if (getById(toId).isEmpty()) {
-            throw new IllegalArgumentException(String.format("There is no account with such id = %s number.", toId));
-        }
-        if (accounts.get(fromId).amount() < amount) {
-            throw new IllegalArgumentException(
-                    String.format(
-                            "The amount on this account id = %s can't be less than the amount you want to transfer.",
-                            fromId));
-        }
-        if (accounts.get(fromId).amount() < amount) {
-            throw new IllegalArgumentException(
-                    String.format(
-                            "The amount on this account id = %s can't be less than the amount you want to transfer.",
-                            toId));
-        }
-        int moneyFrom = getById(fromId).get().amount();
-        int moneyTo = getById(toId).get().amount();
-        if (moneyFrom < amount) {
-            throw new IllegalArgumentException(String.format(
-                    "Amount of the account for transfer can't be less, than transfer sum (%s < transfer sum).", moneyFrom
-            ));
-        }
-        Account accountFrom = getById(fromId).get();
-        Account accountTo = getById(toId).get();
-        accountFrom.setAmount(accountFrom.amount() - amount);
-        accountTo.setAmount(accountTo.amount() + amount);
+        Account accountFrom = getById(fromId).orElseThrow();
+        Account accountTo = getById(toId).orElseThrow();
+        int amountFrom = accountFrom.amount();
+        int amountTo = accountTo.amount();
+        accountFrom.setAmount(amountFrom - amount);
+        accountTo.setAmount(amountTo + amount);
         return true;
     }
 }
