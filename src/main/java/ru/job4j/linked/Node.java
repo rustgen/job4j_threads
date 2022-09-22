3eacde71bdc810da0cf14035489340415d029a87
new_user <nrustgen@gmail.com>
2022-09-21 19:50:41 -0700

    3. Immutable объекты [#267918 #345588]

--- src/main/java/ru/job4j/linked/Node.java
+++ src/main/java/ru/job4j/linked/Node.java
@@ -1,10 +1,10 @@
 package ru.job4j.linked;
 
-public class Node<T> {
+public final class Node<T> {
     private final Node<T> next;
     private final T value;
 
-    public Node(final Node<T> next, final T value) {
+    private Node(Node<T> next, T value) {
         this.next = next;
         this.value = value;
     }
