327a1756c48cefae97f9c49e1cd7b39b64a63430
new_user <nrustgen@gmail.com>
2022-09-23 16:51:07 -0700

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/producerconsumer/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/producerconsumer/SimpleBlockingQueueTest.java
@@ -2,91 +2,48 @@ package ru.job4j.producerconsumer;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SimpleBlockingQueueTest {
 
     @Test
-    void whenAddValuesToQueueTillSize() {
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(200);
         Thread producer = new Thread(
                 () -> {
-                    queue.offer(1);
-                    queue.offer(2);
-                    queue.offer(3);
+                    for (int i = 0; i < 12; i++) {
+                        try {
+                            queue.offer(i);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
                 }
-        );
-        Thread consumer = new Thread(
-                () -> {
-                    assertThat(queue.poll()).isEqualTo(1);
-                    queue.poll();
-                    assertThat(queue.poll()).isEqualTo(3);
-                }
-
-        );
-        assertThat(producer.getState()).isEqualTo(Thread.State.NEW);
-        producer.start();
-        producer.interrupt();
-
-        assertThat(consumer.getState()).isEqualTo(Thread.State.NEW);
-        consumer.start();
-        assertThat(consumer.getState()).isEqualTo(Thread.State.RUNNABLE);
-        consumer.interrupt();
-        try {
-            producer.join(1000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-        assertThat(producer.getState()).isEqualTo(Thread.State.TERMINATED);
-        assertThat(consumer.getState()).isEqualTo(Thread.State.RUNNABLE);
-        try {
-            consumer.join(1000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-        assertThat(consumer.getState()).isEqualTo(Thread.State.TERMINATED);
-    }
-
-    @Test
-    void whenAddValuesToQueueMoreThanSize() {
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
-        Thread producer = new Thread(
-                () -> {
-                    queue.offer(1);
-                    queue.offer(2);
-                    queue.offer(3);
-                    queue.offer(4);
-                    queue.offer(5);
-                    queue.offer(6);
-                }
-        );
-        Thread consumer = new Thread(
-                () -> {
-                    assertThat(queue.poll()).isEqualTo(1);
-                    assertThat(queue.poll()).isEqualTo(2);
-                    assertThat(producer.getState()).isEqualTo(Thread.State.WAITING);
-                    assertThat(queue.poll()).isEqualTo(3);
-                    assertThat(producer.getState()).isEqualTo(Thread.State.BLOCKED);
-                    assertThat(queue.poll()).isEqualTo(4);
-                }
-
         );
         producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
         consumer.start();
-        assertThat(consumer.getState()).isEqualTo(Thread.State.RUNNABLE);
-        producer.interrupt();
+        producer.join();
         consumer.interrupt();
-        try {
-            producer.join(1000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-        try {
-            consumer.join(1000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-        assertThat(consumer.getState()).isEqualTo(Thread.State.BLOCKED);
+        consumer.join();
+        assertThat(buffer).isEqualTo((Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)));
     }
 }
