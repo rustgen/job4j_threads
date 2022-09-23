840357581324e42f7655c9cdd815e7ab6d1150b9
new_user <nrustgen@gmail.com>
2022-09-23 16:27:39 -0700

    2. Обеспечить остановку потребителя. [#66825 #345882]

--- src/main/java/ru/job4j/buffer/ParallelSearch.java
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -10,7 +10,11 @@ public class ParallelSearch {
         final Thread consumer = new Thread(
                 () -> {
                     while (!Thread.currentThread().isInterrupted()) {
+                        try {
                             System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
                     }
                 }
         );
@@ -23,9 +27,9 @@ public class ParallelSearch {
                             Thread.sleep(500);
                         }
                     } catch (InterruptedException e) {
-                        e.printStackTrace();
                         Thread.currentThread().interrupt();
                     }
+                    consumer.interrupt();
                 }
         ).start();
     }
