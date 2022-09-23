9f3c3405e76d0308975cebba89284a2897410328
new_user <nrustgen@gmail.com>
2022-09-23 00:28:51 -0700

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/CASCount.java
@@ -0,0 +1,21 @@
+package ru.job4j;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>();
+
+    public void increment() {
+        int oldNum;
+        int newNum;
+        do {
+            oldNum = count.get();
+            newNum = oldNum++;
+        } while (count.compareAndSet(oldNum, newNum));
+        throw new UnsupportedOperationException("Count is not impl.");
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
