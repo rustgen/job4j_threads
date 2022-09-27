93f4589e7994cc191959439327a856e8f7c340b1
new_user <nrustgen@gmail.com>
2022-09-27 00:23:42 -0700

    3. ForkJoinPool [#315067 #346756]

--- src/test/java/ru/job4j/pools/ParallelFindIndexTest.java
+++ src/test/java/ru/job4j/pools/ParallelFindIndexTest.java
@@ -1,8 +1,5 @@
 package ru.job4j.pools;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,7 +10,7 @@ class ParallelFindIndexTest {
     public void whenObjectOfNumsFindMaxIndexOf4Result12() {
         Object[] nums = new Object[]{1, 2, 3, 56, 76, 2, 4, 98, 324, 453, 23, 1, 4, 24, 53};
         int numSearch = 4;
-        ParallelFindIndex parallelFindIndex = new ParallelFindIndex<>(nums, numSearch, 0, nums.length);
+        ParallelFindIndex<Object> parallelFindIndex = new ParallelFindIndex<>(nums, numSearch, 0, nums.length);
         assertEquals(12, parallelFindIndex.find(nums, numSearch));
     }
 
